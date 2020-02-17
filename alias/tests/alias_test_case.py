70b7439d8a58aa5044b64bf00ca9fc2263dfd375
franklongford <franklongford@gmail.com>
2020-02-17 23:55:03 +0000

    YSY: included AliasTestCase

---
+++ alias/tests/alias_test_case.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+import numpy as np
+
+
+class AliasTestCase(TestCase):
+
+    def assertArrayAlmostEqual(self, array1, array2):
+        return self.assertTrue(np.allclose(array1, array2))
