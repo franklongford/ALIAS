2df9de999240c4da23b38b549073f247c80cae56
franklongford <franklongford@gmail.com>
2020-02-22 16:21:02 +0000

    STY/CLN: more flake8 fixes, remove unused routines

--- alias/src/tests/test_positions.py
+++ alias/src/tests/test_positions.py
@@ -2,7 +2,6 @@ import numpy as np
 import mdtraj as md
 
 from alias.src.positions import (
-    molecules,
     molecular_positions,
     minimum_image,
     coordinate_arrays,
@@ -198,45 +197,3 @@ class TestPositions(AliasTestCase):
 
         with self.assertRaises(AssertionError):
             minimum_image(d_coord, self.cell_dim[:2])
-
-    def test_molecules(self):
-        xat = np.array(
-            [20.3155606, 20.3657056, 19.7335474,
-             20.2454104, 23.1171728, 23.0142095,
-             23.7594160, 23.1883006])
-        yat = np.array(
-            [29.0287238, 28.0910350, 29.3759130,
-             28.9508404, 35.2457050, 34.8579738,
-             34.6865613, 35.1208178])
-        zat = np.array(
-            [58.6756206, 58.8612466, 59.3516029,
-             58.7892616, 63.1022910, 63.9713681,
-             62.6651254, 63.1592576])
-        mol_M = np.array(
-            [16.0, 1.008, 1.008,
-             0.0, 16.0, 1.008,
-             1.008, 0.0])
-
-        answer_xmol = np.array([20.3155606, 23.1171728])
-        answer_ymol = np.array([29.0287238, 35.2457050])
-        answer_zmol = np.array([58.6756206, 63.1022910])
-
-        xmol, ymol, zmol = molecules(xat, yat, zat, 2, 4, mol_M, [0])
-
-        self.assertEqual((2,), xmol.shape)
-        self.assertEqual((2,), ymol.shape)
-        self.assertEqual((2,), zmol.shape)
-
-        self.assertTrue(np.allclose(xmol, answer_xmol))
-        self.assertTrue(np.allclose(ymol, answer_ymol))
-        self.assertTrue(np.allclose(zmol, answer_zmol))
-
-        answer_xmol = np.array([20.28580243, 23.14734565])
-        answer_ymol = np.array([28.99568519, 35.19272710])
-        answer_zmol = np.array([58.72382781, 63.12645656])
-
-        xmol, ymol, zmol = molecules(xat, yat, zat, 2, 4, mol_M, 'COM')
-
-        self.assertTrue(np.allclose(xmol, answer_xmol))
-        self.assertTrue(np.allclose(ymol, answer_ymol))
-        self.assertTrue(np.allclose(zmol, answer_zmol))
