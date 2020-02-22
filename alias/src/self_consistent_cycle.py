38bc6eca813be8b0779639dd34afaa2a574078ed
franklongford <franklongford@gmail.com>
2020-02-22 14:42:23 +0000

    ENH: included command_line_output

--- alias/src/self_consistent_cycle.py
+++ alias/src/self_consistent_cycle.py
@@ -2,6 +2,7 @@ import time
 
 import numpy as np
 
+from alias.io.command_line_output import StdOutTable
 from alias.src.intrinsic_surface import xi
 from alias.src.linear_algebra import update_A_b, lu_decomposition
 from alias.src.spectra import intrinsic_area
@@ -17,11 +18,16 @@ def self_consistent_cycle(
 
     start = time.time()
 
-    print("{:^77s} | {:^43s} | {:^21s} | {:^21s}".format('TIMINGS (s)', 'PIVOTS', 'TAU', 'INT AREA'))
-    print(' {:20s}  {:20s}  {:20s}  {:10s} | {:10s} {:10s} {:10s} {:10s} | {:10s} {:10s} | {:10s} {:10s}'.format(
-        'Matrix Formation', 'LU Decomposition', 'Pivot selection', 'TOTAL', 'n_piv1',
-        '(new)', 'n_piv2', '(new)', 'surf1', 'surf2', 'surf1', 'surf2'))
-    print("_" * 170)
+    stdout_table = StdOutTable()
+    stdout_table.add_section(
+        'TIMINGS (s)',
+        ['Pivot selection', 'Matrix Formation',
+         'LU Decomposition', 'TOTAL'])
+    stdout_table.add_section('PIVOTS', ['n_piv1 (new)', 'n_piv2 (new)'])
+    stdout_table.add_section('TAU', ['surf1', 'surf2'])
+    stdout_table.add_section('INT AREA', ['surf1', 'surf2'])
+
+    print(stdout_table.table_header())
 
     tau1 = tau
     tau2 = tau
@@ -44,7 +50,8 @@ def self_consistent_cycle(
         start1 = time.time()
 
         "Update A matrix and b vector"
-        temp_A, temp_b, fuv = update_A_b(xmol, ymol, zmol, dim, qm, [new_piv1, new_piv2])
+        temp_A, temp_b, fuv = update_A_b(
+            xmol, ymol, zmol, dim, qm, [new_piv1, new_piv2])
 
         A += temp_A
         b += temp_b
@@ -59,9 +66,13 @@ def self_consistent_cycle(
 
         if recon:
             if build_surf1:
-                coeff[0], _ = surface_reconstruction(coeff[0], A[0], b[0], area_diag, curve_matrix, H_var, qm, len(pivot[0]), psi)
+                coeff[0], _ = surface_reconstruction(
+                    coeff[0], A[0], b[0], area_diag, curve_matrix,
+                    H_var, qm, len(pivot[0]), psi)
             if build_surf2:
-                coeff[1], _ = surface_reconstruction(coeff[1], A[1], b[1], area_diag, curve_matrix, H_var, qm, len(pivot[1]), psi)
+                coeff[1], _ = surface_reconstruction(
+                    coeff[1], A[1], b[1], area_diag, curve_matrix,
+                    H_var, qm, len(pivot[1]), psi)
 
         end2 = time.time()
 
@@ -90,18 +101,22 @@ def self_consistent_cycle(
 
         "Calculate distance between molecular z positions and intrinsic surface"
         if build_surf1:
-            zeta_list1 = make_zeta_list(xmol, ymol, zmol, dim, mol_list1, coeff[0], qm, qm)
+            zeta_list1 = make_zeta_list(
+                xmol, ymol, zmol, dim, mol_list1, coeff[0], qm, qm)
         if build_surf2:
-            zeta_list2 = make_zeta_list(xmol, ymol, zmol, dim, mol_list2, coeff[1], qm, qm)
+            zeta_list2 = make_zeta_list(
+                xmol, ymol, zmol, dim, mol_list2, coeff[1], qm, qm)
 
         "Search for more molecular pivot sites"
 
         while finding_pivots:
             "Perform pivot selectrion"
             if piv_search1 and build_surf1:
-                mol_list1, new_piv1, pivot[0] = pivot_selection(mol_list1, zeta_list1, pivot[0], tau1, n0)
+                mol_list1, new_piv1, pivot[0] = pivot_selection(
+                    mol_list1, zeta_list1, pivot[0], tau1, n0)
             if piv_search2 and build_surf2:
-                mol_list2, new_piv2, pivot[1] = pivot_selection(mol_list2, zeta_list2, pivot[1], tau2, n0)
+                mol_list2, new_piv2, pivot[1] = pivot_selection(
+                    mol_list2, zeta_list2, pivot[1], tau2, n0)
 
             "Check whether threshold distance tau needs to be increased"
             if len(new_piv1) == 0 and len(pivot[0]) < n0:
@@ -119,9 +134,11 @@ def self_consistent_cycle(
 
         end = time.time()
 
-        print(' {:20.3f}  {:20.3f}  {:20.3f}  {:10.3f} | {:10d} {:10d} {:10d} {:10d} | {:10.3f} {:10.3f} | {:10.3f} {:10.3f}'.format(
-            end1 - start1, end2 - end1, end - end2, end - start1, len(pivot[0]), len(new_piv1),
-            len(pivot[1]), len(new_piv2), tau1, tau2, area1, area2))
+        print(stdout_table.row([
+            end1 - start1, end2 - end1, end - end2, end - start1,
+            len(pivot[0]), len(new_piv1),
+            len(pivot[1]), len(new_piv2), tau1, tau2, area1, area2]
+        ))
 
     print('\nTOTAL time: {:7.2f} s \n'.format(end - start))
 
@@ -132,13 +149,11 @@ def self_consistent_cycle(
 
 def make_zeta_list(xmol, ymol, zmol, dim, mol_list, coeff, qm, qu):
     """
-    zeta_list(xmol, ymol, dim, mol_list, coeff, qm)
-
-    Calculate dz (zeta) between molecular sites and intrinsic surface for resolution qu"
+    Calculate dz (zeta) between molecular sites and intrinsic
+    surface for resolution qu"
 
     Parameters
     ----------
-
     xmol:  float, array_like; shape=(nmol)
         Molecular coordinates in x dimension
     ymol:  float, array_like; shape=(nmol)
@@ -150,14 +165,14 @@ def make_zeta_list(xmol, ymol, zmol, dim, mol_list, coeff, qm, qu):
     coeff:	array_like (float); shape=(n_waves**2)
         Optimised surface coefficients
     qm:  int
-        Maximum number of wave frequencies in Fouier Sum representing intrinsic surface
+        Maximum number of wave frequencies in Fouier Sum
+        representing intrinsic surface
 
     Returns
     -------
-
     zeta_list:  float, array_like; shape=(n0)
-        Array of dz (zeta) between molecular sites and intrinsic surface
-
+        Array of dz (zeta) between molecular sites and
+        intrinsic surface
     """
 
     "Calculate shortest z distance between molecules and surface, zeta"
@@ -172,29 +187,30 @@ def make_zeta_list(xmol, ymol, zmol, dim, mol_list, coeff, qm, qu):
 
 def pivot_selection(mol_list, zeta_list, piv_n, tau, n0):
     """
-    pivot_selection(mol_list, zeta_list, piv_n, tau, n0)
-
-    Search through zeta_list for values within tau threshold and add to pivot list
+    Search through zeta_list for values within tau threshold
+    and add to pivot list
 
     Parameters
     ----------
-
     mol_list:  int, array_like; shape=(n0)
         Indices of molcules available to be selected as pivots
     zeta_list:  float, array_like; shape=(n0)
-        Array of dz (zeta) between molecular sites and intrinsic surface
+        Array of dz (zeta) between molecular sites and intrinsic
+        surface
     piv_n:  int, array_like; shape=(n0)
         Molecular pivot indices
     tau:  float
-        Threshold length along z axis either side of existing intrinsic surface for selection of new pivot points
+        Threshold length along z axis either side of existing intrinsic
+        surface for selection of new pivot points
     n0:  int
-        Maximum number of molecular pivots in intrinsic surface
+        Maximum number of molecular pivots in intrinsic
+        surface
 
     Returns
     -------
-
     mol_list:  int, array_like; shape=(n0)
-        Updated indices of molcules available to be selected as pivots
+        Updated indices of molcules available to be selected as
+        pivots
     new_piv:  int, array_like
         Indices of new pivot molecules just selected
     piv_n:  int, array_like; shape=(n0)
@@ -202,14 +218,15 @@ def pivot_selection(mol_list, zeta_list, piv_n, tau, n0):
 
     """
 
-    "Find new pivots based on zeta <= tau"
+    # Find new pivots based on zeta <= tau
     new_piv = mol_list[zeta_list <= tau]
     dz_new_piv = zeta_list[zeta_list <= tau]
 
-    "Order pivots by zeta (shortest to longest)"
+    # Order pivots by zeta (shortest to longest)
     bubble_sort(new_piv, dz_new_piv)
 
-    "Add new pivots to pivoy list and check whether max n0 pivots are selected"
+    # Add new pivots to pivoy list and check whether max n0
+    # pivots are selected"
     piv_n = np.concatenate((piv_n, new_piv))
 
     if piv_n.shape[0] > n0:
@@ -218,10 +235,11 @@ def pivot_selection(mol_list, zeta_list, piv_n, tau, n0):
 
     far_tau = 6.0 * tau
 
-    "Remove pivots far from molecular search list"
+    # Remove pivots far from molecular search list
     far_piv = mol_list[zeta_list > far_tau]
     if len(new_piv) > 0:
-        mol_list = numpy_remove(mol_list, np.concatenate((new_piv, far_piv)))
+        mol_list = numpy_remove(
+            mol_list, np.concatenate((new_piv, far_piv)))
 
     assert np.sum(np.isin(new_piv, mol_list)) == 0
 
@@ -230,15 +248,17 @@ def pivot_selection(mol_list, zeta_list, piv_n, tau, n0):
 
 def initialise_surface(qm, phi, dim, recon=False):
     """
-    Calculate initial parameters for ISM and reconstructed ISM fitting procedure
+    Calculate initial parameters for ISM and reconstructed
+    ISM fitting procedure
 
     Parameters
     ----------
-
     qm:  int
-        Maximum number of wave frequencies in Fouier Sum representing intrinsic surface
+        Maximum number of wave frequencies in Fourier Sum
+        representing intrinsic surface
     phi:  float
-        Weighting factor of minimum surface area term in surface optimisation function
+        Weighting factor of minimum surface area term in surface
+        optimisation function
     dim:  float, array_like; shape=(3)
         XYZ dimensions of simulation cell
     recon:  bool (optional)
@@ -250,10 +270,13 @@ def initialise_surface(qm, phi, dim, recon=False):
     coeff:	array_like (float); shape=(n_waves**2)
         Optimised surface coefficients
     A:  float, array_like; shape=(n_waves**2, n_waves**2)
-        Matrix containing wave product weightings f(x, u1, Lx).f(y, v1, Ly).f(x, u2, Lx).f(y, v2, Ly)
-        for each coefficient in the linear algebra equation Ax = b for both surfaces
+        Matrix containing wave product weightings
+        f(x, u1, Lx).f(y, v1, Ly).f(x, u2, Lx).f(y, v2, Ly)
+        for each coefficient in the linear algebra equation
+        Ax = b for both surfaces
     b:  float, array_like; shape=(n_waves**2)
-        Vector containing solutions z.f(x, u, Lx).f(y, v, Ly) to the linear algebra equation Ax = b
+        Vector containing solutions z.f(x, u, Lx).f(y, v, Ly)
+        to the linear algebra equation Ax = b
         for both surfaces
     area_diag: float, array_like; shape=(n_waves**2)
         Surface area diagonal terms for A matrix
